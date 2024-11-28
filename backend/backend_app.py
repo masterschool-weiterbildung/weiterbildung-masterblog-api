621c491b48ae66c7ff87843eb7c7e423cdb28ee8
Jerome de Dios <182743031+jdedios-de@users.noreply.github.com>
2024-11-28 14:50:03 +0100

    Adds - Add Endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify
+import datetime
+
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from marshmallow import Schema, fields, ValidationError
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -10,8 +13,56 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+def standard_error_response(status, error, error_code, message, details,
+                            path):
+    return {
+        "status": f"{status}",
+        "error": f"{error}",
+        "errorCode": f"{error_code}",
+        "message": f"{message}",
+        "details": f"{details}",
+        "path": f"{path}"
+    }
+
+
+def jsonify_response(payload):
+    return {
+        "posts": payload,
+        "createdAt": datetime.datetime.now().isoformat()
+    }
+
+
+class ItemSchema(Schema):
+    title = fields.Str(required=True)
+    content = fields.Str(required=True)
+
+
+schema = ItemSchema()
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
+    if request.method == 'POST':
+        try:
+            new_post = schema.load(request.get_json())
+
+            new_id = max(post['id'] for post in POSTS) + 1
+            new_post['id'] = new_id
+
+            POSTS.append(new_post)
+
+            return jsonify(jsonify_response(new_post)), 201
+        except ValidationError as err:
+            return jsonify(
+                standard_error_response(400,
+                                        "Bad Request",
+                                        "VALIDATION_ERROR_MISSING_TITLE_CONTENT",
+                                        "Validation failed.",
+                                        f"{err}",
+                                        "/api/posts"
+
+                                        )), 400
+
     return jsonify(POSTS)
 
 
